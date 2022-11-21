b950a0597f683dd26f1981c1e73db2639990d5ad
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-11-20 23:42:42 -0300

    Initial commit

---
+++ Variaveis/script.js
@@ -0,0 +1,36 @@
+/* console.log('Já sei só quero relembrar') */
+
+/* var nome = 'Caio Mouta';
+var idade = 49
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade); */
+
+var preco = 21
+var totalComprado = 5
+var totalPreco = preco * totalComprado
+
+console.log(totalPreco)
+
+var sobrenome = "Lorem". cidade = "Otawa"
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir)
+
+var food;
+console.log(food)
+food = "Lorem"
+console.log(food)
+
+// Declarar Variavel com nome 
+var nome = 'Lorem Ipsum'
+
+// Variavel idade
+
+var idade = 21  
+
+var food;
+console.log(food)
+food = "Lorem"
