40b4d66bd9a97ad37b828589c4ddce4d65889a15
CAIO MOUTA <caioeduardo1993@gmail.com>
2023-01-21 19:29:07 -0300

    traversing

--- transversing-e-manipulacao/script.js
+++ transversing-e-manipulacao/script.js
@@ -1,2 +1,22 @@
 const h1 = document.querySelector('h1');
-const animaisLista = document.querySelector('.animais.lista');
+const animaisLista = document.querySelector('.animais.descricao');
+console.log(h1.innerHTML)
+console.log(h1.outerHTML)
+
+h1.innerHTML = "<p>Novo Titulo</p>"
+
+console.log(animaisLista.innerText);
+
+const lista = document.querySelector('.animais-lista');
+
+console.log(lista.children(lista.children.length -1));
+
+console.log(lista.querySelector('li-last-child'));
+
+console.log(lista.previousSibling);
+
+const animais = document.querySelector('.animais');
+const contato = document.querySelector('.contato');
+const titulo = contato.querySelector('.titulo');
+
+lista.appendChild(titulo);
