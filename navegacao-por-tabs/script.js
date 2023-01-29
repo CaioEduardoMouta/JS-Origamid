51554b1037e9932ff91db96ad08589e287dcdc42
CAIO MOUTA <caioeduardo1993@gmail.com>
2023-01-29 10:30:08 -0300

    navegation-tabs

---
+++ navegacao-por-tabs/script.js
@@ -0,0 +1,15 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu');
+const tabContent = document.querySelectorAll('js-tabcontent li');
+
+function activeTab(index) {
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    })
+    tabContent(index).classList.add('ativo');
+}
+
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index);
+    })
+});
