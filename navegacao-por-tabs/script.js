e3809f6221871f003801ff149e964caf38f1457a
CAIO MOUTA <caioeduardo1993@gmail.com>
2023-02-05 10:59:09 -0300

    acordion-e-scroll

--- navegacao-por-tabs/script.js
+++ navegacao-por-tabs/script.js
@@ -1,15 +1,20 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu');
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('js-tabcontent li');
 
-function activeTab(index) {
-    tabContent.forEach((section) => {
-        section.classList.remove('ativo');
-    })
-    tabContent(index).classList.add('ativo');
-}
+if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
 
-tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    })
-});
+
+    function activeTab(index) {
+        tabContent.forEach((section) => {
+            section.classList.remove('ativo');
+        })
+        tabContent(index).classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        });
+    });
+}
