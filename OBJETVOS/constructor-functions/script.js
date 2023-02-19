815e2b04d57d7d114949e498c26f8c967459d82e
CAIO MOUTA <caioeduardo1993@gmail.com>
2023-02-19 18:52:48 -0300

    JS

--- OBJETVOS/constructor-functions/script.js
+++ OBJETVOS/constructor-functions/script.js
@@ -1,17 +1,11 @@
-function Carro(marcaAtribuida,precoAtribuido){
-    this.marca = marcaAtribuida;
-    this.preco = precoAtribuido;
+const Dom = {
+    seletor: 'li',
+    elemento(){
+        const elementoSelecionado = document.querySelector(this.seletor);
+        return elementoSelecionado;
+    },
+    ativar() {
+        const elementoSelecionado = this.element();
+        elementoSelecionado.classList.add('ativar');
+    }
 }
-
-
-const honda = new Carro('Honda',3000);
-const fiat = new Carro('Fiat',4005);
-
-function Carro2(marca,precoInicial){
-    const taxa = 1.2;
-    const precoFinal = precoInicial * taxa;
-    this.marca = marca;
-    this.preco = precoInicial;
-}
-
-const mazda = new Carro2('Mazda',6000);
