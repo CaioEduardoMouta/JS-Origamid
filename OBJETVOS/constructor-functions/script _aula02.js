09a419fbde8893508612c65c0bc5275faf53cd89
CAIO MOUTA <caioeduardo1993@gmail.com>
2023-02-20 13:43:07 -0300

    ExerciciosOBJ

---
+++ OBJETVOS/constructor-functions/script _aula02.js
@@ -0,0 +1,26 @@
+/* const Dom = {
+    seletor: 'li',
+    elemento(){
+        return elementoSelecionado;
+    },
+    ativar() {
+        elementoSelecionado.classList.add('ativar');
+    }
+} */
+
+function Dom(seletor ) {
+    this.element = function (){
+        return document.querySelector(seletor);
+    },
+    this.ativar = function (classe) {
+        this.element().classList.add(classe);
+    }
+}
+
+
+const li = new Dom('li');
+const ul = new Dom('ul');
+
+const lastLi = new Dom('li:last-child');
+lastLi.ativar('ativar');
+
