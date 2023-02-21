f7147463a0a6d7eba740b8cdc17bff250c221037
CAIO MOUTA <caioeduardo1993@gmail.com>
2023-02-21 17:12:39 -0300

    JS

---
+++ OBJETVOS/native-host-e-user/exercicios.js
@@ -0,0 +1,16 @@
+// Liste 5 objetos nativos
+Object 
+String 
+Array
+Function
+Number
+// Liste 5 objetos do browser
+window
+history
+document
+HTMLCollection
+NodeList
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+webkitURL
+webkitHidden
---
+++ OBJETVOS/native-host-e-user/script.js
@@ -0,0 +1,7 @@
+console.log(typeof Array.fromgagasfgas !== "undefined");
+
+if(typeof Array.fromgagasfgas !=="undefined") {
+    console.log('Existe')
+} else {
+    console.log('Ñ Existe')
+}
