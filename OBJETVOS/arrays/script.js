57498ed721b83957c37226b1c22c3ab90ddda557
CAIO MOUTA <caioeduardo1993@gmail.com>
2023-03-05 20:21:29 -0300

    Array

--- OBJETVOS/arrays/script.js
+++ OBJETVOS/arrays/script.js
@@ -1,4 +1,4 @@
-
+/* 
 const precos = [49, 99, 69, 89];
 
 const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], 
@@ -42,3 +42,36 @@ const idade = [32,21,22,59,2,9,11];
 
 console.log(instrumentos)
 console.log(idade)
+ */
+
+
+const carros = ['Ford','Fiat','VW'];
+carros.unshift('Kia','Ferrari');
+carros.push('Kia','Porsche');
+
+console.log(carros);~
+
+console.log(carros.pop());
+
+console.log(carros.shift());
+
+console.log(carros.splica(2,0,'Fusca'))
+
+console.log(carros);
+
+
+console.log(['Lorem1','Lorem2','Lorem3','Lorem4','Lorem5'].copyWithin(2,0,2))
+
+
+const transporte1 = ['Barco', 'Aviao'];
+const transporte2 = ['Carro', 'Moto'];
+
+const linguagens = ['PHP','Python','Java','JS','Ruby'];
+
+let htmlString = '<h2>Titulo Principal</h2>';
+htmlString = htmlString.split('h2');
+htmlString = htmlString.join('h1')
+
+console.log(htmlString);
+
+console.log(linguagens.slice(2,4))
